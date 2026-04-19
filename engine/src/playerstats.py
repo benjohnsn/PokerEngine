05f1708c4229a707730dbfe0fe6e10bdeb0af0fa
Ben Johnson <157973223+benjohnsn@users.noreply.github.com>
2026-04-19 23:45:31 +0100

    Add PlayerStats class to track basic poker statistics

---
+++ engine/src/playerstats.py
@@ -0,0 +1,17 @@
+class PlayerStats:
+    def __init__(self):
+        self.hands = 0
+
+        self.vpip = 0
+        self.vpipOpps = 0
+
+        self.pfr = 0
+        self.pfrOpps = 0
+
+        self.bets = 0
+        self.raises = 0
+        self.calls = 0
+        self.folds = 0
+
+        self.showdowns = 0
+        self.showdownWins = 0
